eff0d108545783411d4f5ed7872c18db7b238e7d
ckhall267 <ton_email_github>
2025-10-17 00:25:23 +0100

    first commit

---
+++ src/main/java/entities/Salle.java
@@ -0,0 +1,19 @@
+package entities;
+
+import java.util.List;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "salles")
+public class Salle {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String code;
+
+    @OneToMany(mappedBy = "salle", fetch = FetchType.EAGER)
+    private List<Machine> machines;
+
+    // Constructeurs, getters et setters
+}
